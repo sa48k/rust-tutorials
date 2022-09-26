95c54534eab08ba9fe596219c02e8ca11e543690
sa48k <sa48k@users.noreply.github.com>
2022-09-26 19:59:15 +1300

    Update main.rs

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,30 +6,34 @@ fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
-
+    let mut attempts: u32 = 0;
     // println!("The secret number is {}", secret_number); // debug
 
     loop {
         println!("Input your guess:");
 
         let mut guess = String::new();
-
+        
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
 
         let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
+            Ok(num) => {
+                attempts = attempts + 1;
+                num
+            },
             Err(_) => continue,
         };
 
-        println!("You guessed: {guess}");
+        // println!("You guessed: {guess}");
+        // println!("You have had {attempts} attempts");
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too low!"),
             Ordering::Greater => println!("Too high!"),
             Ordering::Equal => {
-                println!("You win!");
+                println!("You win in {attempts} attempts!");
                 break;
             }
         }
