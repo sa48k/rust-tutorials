44a9a6a94b1b2a9b1e968a1e44d3384c2c7c2f09
sa48k <sa48k@users.noreply.github.com>
2022-10-29 21:06:19 +1300

    Ownership and Slices 1

---
+++ ownership.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let s1 = gives_ownership();
+    let s2 = String::from("hello");
+    let s3 = takes_and_gives_back(s2);
+
+    println!("{} {}", s1, s3);      // s2 is moved and is no longer available
+}
+
+fn gives_ownership() -> String {
+    let some_string = String::from("yours");
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    
+    a_string
+}
---
+++ test.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let s1 = String::from("hello");
+    let _s2 = s1.clone();
+    
+    println!("{}", s1);
+}
